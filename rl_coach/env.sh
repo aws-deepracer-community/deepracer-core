ff2249cb5ae8864d01d8983365f840a9b58904e4
crr0004 <crr0004@gmail.com>
2019-04-10 20:46:23 +1000

    Added env.sh files for easily setting envionment variables in a shell

---
+++ rl_coach/env.sh
@@ -0,0 +1,10 @@
+export AWS_ACCESS_KEY_ID=minio
+export AWS_SECRET_ACCESS_KEY=miniokey
+export WORLD_NAME=hard_track
+export ROS_AWS_REGION=us-east-1
+export AWS_REGION=us-east-1
+export MODEL_S3_PREFIX=rl-deepracer-sagemaker
+export MODEL_S3_BUCKET=bucket
+export LOCAL=True
+export S3_ENDPOINT_URL=http://10.1.0.8:9000
+export MARKOV_PRESET_FILE=deepracer.py
