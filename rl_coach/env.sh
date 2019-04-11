b8473a67e2ea962ad0f633fca6fb82585b5217dc
crr0004 <crr0004@gmail.com>
2019-04-11 18:36:28 +1000

    Add more options to env.sh file

--- rl_coach/env.sh
+++ rl_coach/env.sh
@@ -1,8 +1,11 @@
+export MINIO_ACCESS_KEY=minio
+export MINIO_SECRET_KEY=miniokey
 export AWS_ACCESS_KEY_ID=minio
 export AWS_SECRET_ACCESS_KEY=miniokey
 export WORLD_NAME=hard_track
 export ROS_AWS_REGION=us-east-1
 export AWS_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
 export MODEL_S3_PREFIX=rl-deepracer-sagemaker
 export MODEL_S3_BUCKET=bucket
 export LOCAL=True
