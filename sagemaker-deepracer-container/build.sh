40e239eeaaede110b200bbf8a27451d707f2da0a
Lars Ludvigsen <lars@ludvig.no>
2020-02-14 21:26:59 +0000

    Updated build-script

--- sagemaker-deepracer-container/build.sh
+++ sagemaker-deepracer-container/build.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
+trap ctrl_c INT
 
-#mkdir -p staging 
+function ctrl_c() {
+        echo "Requested to stop."
+        exit 1
+}
 
-cd ../sagemaker-containers/
-#python setup.py sdist
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/sagemaker-tensorflow-container/
+python setup.py sdist
+cp dist/*.tar.gz docker/build_artifacts/
+git apply $DIR/lib/dockerfile-cudnn.patch
+cd docker/build_artifacts
+
+for arch in cpu gpu; do
+    docker build . -t local/sagemaker-tensorflow-container:$arch -f ../1.15.0/py3/Dockerfile.$arch --build-arg py_version=3
+done
+rm *.tar.gz
+
+cd $DIR/sagemaker-tensorflow-container/
+git apply --reverse ../lib/dockerfile-cudnn.patch
+
+## Second stage
+rm -rf $DIR/staging
+mkdir -p $DIR/staging 
+cd $DIR/sagemaker-containers/
+python setup.py sdist
 SAGEMAKER_CONTAINER_DIST=$(ls ./dist/)
-#cp ./dist/$SAGEMAKER_CONTAINER_DIST ../sagemaker-deepracer-container/staging/
-cd ../sagemaker-deepracer-container
-#cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/lib staging/
-#cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/markov staging/
-#cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/rl_coach.patch staging/
+cp ./dist/$SAGEMAKER_CONTAINER_DIST $DIR/staging/
 
-for arch in nvidia;
+cd $DIR
+cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/lib staging/
+cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/markov staging/
+cp -r ../amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/rl_coach.patch staging/
+
+for arch in cpu gpu;
 do
     docker build -t local/sagemaker-deepracer-container:$arch . --build-arg sagemaker_containers=$SAGEMAKER_CONTAINER_DIST --build-arg arch=$arch
 done
