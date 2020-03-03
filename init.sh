1edde6b4bfa3f0fd3552f5f71b60c661a6eb429c
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-03-03 18:46:34 +0000

    Updating install-script

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+git submodule init && git submodule update
 sudo apt-get install python3-dev python3-venv
 python3 -m venv .env --prompt deepracer
 source .env/bin/activate
 
-pip install awscli sagemaker wheel setuptools pandas
+pip install awscli wheel setuptools pandas
+cd dependencies/sagemaker-python-sdk && python setup.py sdist && pip install dist/*.tar.gz && cd ../..
