44f987c803eb656ce0a07f75cd0a17afae709038
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-03-04 15:57:35 +0000

    Configuring minio to run as active user

--- minio.sh
+++ minio.sh
@@ -1,6 +1,8 @@
 export MINIO_ACCESS_KEY=minio
 export MINIO_SECRET_KEY=miniokey
 
-docker run --rm -d -p 9000:9000 -e "MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY" -e "MINIO_SECRET_KEY=$MINIO_SECRET_KEY" --name minio1 \
-  -v /home/deepracer/minio:/data \
+mkdir ~/minio/bucket
+
+docker run --rm -u `id -u` -d -p 9000:9000 -e "MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY" -e "MINIO_SECRET_KEY=$MINIO_SECRET_KEY" --name minio1 \
+  -v ~/minio:/data --name minio \
   minio/minio server /data
