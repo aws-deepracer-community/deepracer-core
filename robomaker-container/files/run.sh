a5c05c35dc8aa8b066d2a08076fe60608d64db15
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-03-05 16:00:28 +0000

    Updating robomaker -- initial test works

--- robomaker-container/files/run.sh
+++ robomaker-container/files/run.sh
@@ -12,12 +12,13 @@ if [ -z ${2+x} ]; then
 	exit
 
 fi
-source install/setup.sh
+# source install/setup.sh
+source setup.bash
 if which x11vnc &>/dev/null; then
 	export DISPLAY=:0 # Select screen 0 by default.
 	xvfb-run -f $XAUTHORITY -l -n 0 -s ":0 -screen 0 1400x900x24" jwm &
 	x11vnc -bg -forever -nopw -rfbport 5900 -display WAIT$DISPLAY &
-	roslaunch deepracer_simulation $2 &
+	roslaunch deepracer_simulation_environment $2 &
 	rqt &
 	rviz &
 fi
