021d4c121237307ed212fccff8b7867dd28ce403
Lars Ludvigsen <lars@ludvig.no>
2020-03-22 14:10:53 +0000

    Making config director

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo apt-get install python3-dev python3-venv -y
 python3 -m venv .env --prompt deepracer
 source .env/bin/activate
 
+mkdir -p config
+
 pip install awscli wheel setuptools pandas
 cd dependencies/sagemaker-python-sdk && python setup.py sdist && pip install dist/*.tar.gz && cd ../..
 
