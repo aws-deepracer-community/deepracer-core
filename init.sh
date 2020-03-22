0dbf15e5fd9c90b69faae9e600d89706f971fb2d
Lars Ludvigsen <lars@ludvig.no>
2020-03-22 13:30:04 +0000

    Coach file clean-up

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ source .env/bin/activate
 
 pip install awscli wheel setuptools pandas
 cd dependencies/sagemaker-python-sdk && python setup.py sdist && pip install dist/*.tar.gz && cd ../..
+
+ln -sf dependencies/amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/common/ rl-coach-container/files/common
+ln -sf robomaker-container/bundle/sagemaker_rl_agent/lib/python3.5/site-packages/markov/ rl-coach-container/files/markov
