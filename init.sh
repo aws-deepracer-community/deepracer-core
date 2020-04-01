7c177e5c72cb472e58dc0f2809b9d8652c719a0b
Lars Ludvigsen <lars@ludvig.no>
2020-04-01 19:12:54 +0000

    Reverting to stock sagemaker-python-sdk

--- init.sh
+++ init.sh
@@ -6,9 +6,9 @@ source .env/bin/activate
 
 mkdir -p config rl-coach-container/staging
 
-pip install awscli wheel setuptools pandas
-cd dependencies/sagemaker-python-sdk && python setup.py sdist && pip install dist/*.tar.gz && cd ../..
+pip install awscli wheel setuptools pandas sagemaker
+# cd dependencies/sagemaker-python-sdk && python setup.py sdist && pip install dist/*.tar.gz && cd ../..
 
 cp -R dependencies/amazon-sagemaker-examples/reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/common/ rl-coach-container/files/common
 cp -R robomaker-container/bundle/sagemaker_rl_agent/lib/python3.5/site-packages/markov/ rl-coach-container/files/markov
-cp dependencies/sagemaker-python-sdk/dist/*.tar.gz rl-coach-container/staging
+# cp dependencies/sagemaker-python-sdk/dist/*.tar.gz rl-coach-container/staging
